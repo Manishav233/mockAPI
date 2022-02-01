98b812d225e9fd6d1491d80868f948ea1d0dc12c
Manishav233 <manishav233@gmail.com>
2022-02-01 20:33:08 +0530

    updated till get method

--- users.js
+++ users.js
@@ -1,27 +1,71 @@
-document.body.innerHTML=`<section class="user-list"> </section>`;
+ document.body.innerHTML=`<section class="user-list"> </section>`;
 async function getAllUsers(){
 const data=await fetch("https://617062da23781c0017289a16.mockapi.io/users" ,{method:"GET"});
 const users=await data.json();
 const userContainer=document.querySelector(".user-list");
 
-userContainer.innerHTML= "";
-
 users.forEach((user)=>{
-userContainer.innerHTML+=  `
+userContainer.innerHTML+= `
 <div class="user-container">
 <img class="user-avatar" src=${user.avatar} alt=${user.name}/>
-<div>
+
+
 <p class="user-name">${user.name}</p>
-<button onclick="deleteUser(${user.id})">DELETE</button>
-</div>
+
+
 </div>`;
 });
 console.log(users);
 }
 getAllUsers();
 
-async function deleteUser(userId){
-console.log("deleting", userId);
-    const data=await fetch("https://617062da23781c0017289a16.mockapi.io/users/" +userId,{method:"DELETE"});
-};
-getAllUsers();
+// async function deleteUser(userId){
+// console.log("deleting", userId);
+//     const data=await fetch("https://617062da23781c0017289a16.mockapi.io/users/" +userId,{method:"DELETE"});
+// };
+// getAllUsers();
+
+// document.body.innerHTML=`<section class="profile"></section>`;
+
+// async function Myown(){
+
+
+// const data=await fetch("https://617062da23781c0017289a16.mockapi.io/users");
+// const user=await data.json();
+// console.log(user);
+// user.forEach((u)=>{
+// console.log(u.avatar);
+// const userContainer=document.querySelector(".profile");
+// userContainer.innerHTML+=
+// `
+// <img src="${u.avatar}" alt="profile"/>
+// <h1>${u.name}</h1>
+// `
+// });
+// }
+// Myown();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
